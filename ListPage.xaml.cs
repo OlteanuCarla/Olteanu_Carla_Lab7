c051d34c452f5c05c10ee187c2af6473671e7f0f
CARLA-ANTONIA OLTEANU <carla.olteanu@stud.ubbcluj.ro>
2023-12-15 20:14:21 +0200

    Laborator10

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,5 +1,5 @@
-using Olteanu_Carla_Lab7.Models;
 namespace Olteanu_Carla_Lab7;
+using Olteanu_Carla_Lab7.Models;
 
 public partial class ListPage : ContentPage
 {
@@ -11,6 +11,8 @@ public partial class ListPage : ContentPage
     {
         var slist = (ShopList)BindingContext;
         slist.Date = DateTime.UtcNow;
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+        slist.ShopID = selectedShop.ID;
         await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
@@ -32,6 +34,9 @@ public partial class ListPage : ContentPage
     protected override async void OnAppearing()
     {
         base.OnAppearing();
+        var items = await App.Database.GetShopsAsync();
+        ShopPicker.ItemsSource = (System.Collections.IList)items;
+        ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
         var shopl = (ShopList)BindingContext;
 
         listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
